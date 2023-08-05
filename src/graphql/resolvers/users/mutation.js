921a9abe76d53a55786a411696d10b737c0b1e32
Joel Alvarez Mexia <jeresoft@gmail.com>
2023-08-05 14:06:00 -0700

    feat(reolvers): update a user

--- src/graphql/resolvers/users/mutation.js
+++ src/graphql/resolvers/users/mutation.js
@@ -1,5 +1,9 @@
 const { addUser } = require('./addUser');
+const { deleteUser } = require('./deleteUser');
+const { updateUser } = require('./updateUser');
 
 module.exports = {
   addUser,
+  deleteUser,
+  updateUser,
 };
---
+++ src/graphql/resolvers/users/updateUser.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-underscore-dangle */
+const { updateUser } = require('./updateUser');
+const { connectDB, disconnectDB } = require('../../../db/connectDB');
+const usersData = require('../../../../data/users.json');
+const User = require('../../../models/user');
+
+describe('Update a user unit test', () => {
+  let idUser = null;
+
+  beforeAll(async () => {
+    await connectDB();
+    let users = await User.find({});
+    if (users.length === 0) {
+      await User.insertMany(usersData);
+      users = await User.find({});
+    }
+    idUser = users[0].id;
+  });
+
+  afterAll(async () => {
+    await disconnectDB();
+  });
+
+  it('returns a user data if the user was updated', async () => {
+    const user = {
+      id: idUser,
+      name: 'Ernesto',
+      lastName: 'Gomez',
+    };
+    const response = await updateUser(null, user);
+
+    expect(response).toMatchObject(user);
+  });
+});
