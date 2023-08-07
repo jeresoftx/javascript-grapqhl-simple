5316fa4b339bcb5ee64a3f16fc2dd5f37c38ee8d
Joel Alvarez Mexia <jeresoft@gmail.com>
2023-08-07 13:46:32 -0700

    feat(reolvers): delete permission

--- src/graphql/resolvers/permissions/mutation.js
+++ src/graphql/resolvers/permissions/mutation.js
@@ -1,7 +1,9 @@
 const { addPermission } = require('./addPermission');
 const { updatePermission } = require('./updatePermission');
+const { deletePermission } = require('./deletePermission');
 
 module.exports = {
   addPermission,
   updatePermission,
+  deletePermission,
 };
