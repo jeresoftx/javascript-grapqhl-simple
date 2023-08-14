aa01a0d4477ff66e7daf3c9f702ca06ebdacfcbc
Joel Alvarez Mexia <jeresoft@gmail.com>
2023-08-14 15:51:04 -0700

    feat(middleware): auth

--- __tests__/unit/resolvers/version/version.test.js
+++ __tests__/unit/resolvers/version/version.test.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 const {
-  version,
+  versionQ,
 } = require('../../../../src/graphql/resolvers/version/version');
 
 describe('Graphql version', () => {
   it('Should return the graphql current version', async () => {
-    const response = await version();
+    const response = await versionQ();
     expect(response).toMatch(
       /graphql-javacript-simple - v\d\.\d\.\d A simple graphql in javascript using apollo server/,
     );
