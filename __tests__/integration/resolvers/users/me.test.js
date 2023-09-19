be59457b59debaea4a1be24f193a4133ba744477
Joel Alvarez Mexia <139817810+jeresoftx@users.noreply.github.com>
2023-09-19 00:34:55 -0700

    ci: :bug: remove token jwt
    Fix #128

--- __tests__/integration/resolvers/users/me.test.js
+++ __tests__/integration/resolvers/users/me.test.js
@@ -27,10 +27,7 @@ describe('Me unit test', () => {
       .post('/')
       .set('Content-Type', 'application/json')
       .set('user-agent', 'jest')
-      .set(
-        'Authorization',
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDY2YmMwYWExY2EyZTZkY2EwNTk3Y2IiLCJ1c2VybmFtZSI6ImplcmVzb2Z0IiwiaWF0IjoxNjk1MTA0Mjc0LCJleHAiOjE2OTUxOTA2NzR9.ipXRtxxGKrUZPc0O8IgLjSoAJJrLpRfuY01ly6uESg4',
-      )
+      .set('Authorization', `Bearer ${process.env.TOKEN_JWT}`)
       .send({
         query: queryMe,
       });
